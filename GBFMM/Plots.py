2d738dd6261a698f5f79239d5a4337fd9ce6fa1a
joshua-james-f <joshua.james.f@gmail.com>
2015-04-24 07:08:52 -0400

    Made plot script adapt to number of latent calsses

--- GBFMM/Plots.py
+++ GBFMM/Plots.py
@@ -4,12 +4,58 @@
 from ggplot import *
 # 
 def plot_traj(outs):
-    d = {'one' : outs[0],
-     'two' : outs[1],
-     'time': range(1,7)}
-    dat = pd.DataFrame(d)
-    dm = pd.melt(dat, id_vars=['time'])
-    print ggplot(dm, aes('time', 'value', color='variable')) + \
-        geom_point(size=40) + \
-        geom_line(size=3) + \
-        theme_bw()
+    if len(outs) == 2:
+        d = {'one' : outs[0], 
+        'two' : outs[1], 
+        'Time': range(1,len(outs[0])+1)}
+    elif len(outs) == 3:
+	    d = {'one' : outs[0], 
+	    'two' : outs[1], 
+	    'three' : outs[2], 
+	    'Time': range(1,len(outs[0])+1)}
+    elif len(outs) == 4: 
+    	d = {'one' : outs[0], 
+    	'two' : outs[1], 
+    	'three' : outs[2], 
+    	'four' : outs[3], 
+    	'Time': range(1,len(outs[0])+1)}
+    elif len(outs) == 5:
+    	d = {'one' : outs[0], 
+    	'two' : outs[1], 
+    	'three' : outs[2], 
+    	'four' : outs[3], 
+    	'five' : outs[4], 
+    	'Time': range(1,len(outs[0])+1)}
+    elif len(outs) == 6:
+    	d = {'one' : outs[0], 
+    	'two' : outs[1], 
+    	'three' : outs[2], 
+    	'four' : outs[3], 
+    	'five' : outs[4], 
+    	'six' : outs[5], 
+    	'Time': range(1,len(outs[0])+1)}
+    elif len(outs) == 7: 
+    	d = {'one' : outs[0],
+    	'two' : outs[1], 
+    	'three' : outs[2], 
+    	'four' : outs[3], 
+    	'five' : outs[4], 
+    	'six' : outs[5], 
+    	'seven' : outs[6], 
+    	'Time': range(1,len(outs[0])+1)}
+    elif len(outs) == 8:
+    	d = {'one' : outs[0],
+    	'two' : outs[1], 
+    	'three' : outs[2], 
+    	'four' : outs[3], 
+    	'five' : outs[4], 
+    	'six' : outs[5], 
+    	'seven' : outs[6], 
+    	'eight' : outs[7], 
+    	'Time': range(1,len(outs[0])+1)}
+    dat = pd.DataFrame(d) 
+    dm = pd.melt(dat, id_vars=['Time'], var_name='Latent Class', value_name='Value') 
+    print ggplot(dm, aes('Time', 'Value', color='Latent Class')) + \
+    	geom_point(size=80) + \
+    	geom_line(size=3) + \
+    	theme_bw()
